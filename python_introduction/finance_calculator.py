a1c5185b29a2f3d61b9b90c9b531a1215bf820e6
Sechaba Mtambo <mtambosechaba@gmail.com>
2025-07-09 12:35:20 +0200

    Fix finance_calculator.py to use correct variable names and output format

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,7 +1,7 @@
-monthlyIncome = float(input("Enter your monthly income: "))
-monthlyExpenses = float(input("Enter your total monthly expenses: "))
-monthlySavings = monthlyIncome - monthlyExpenses
-projectedSavings = monthlySavings * 12 + (monthlySavings * 12 * 0.05)
+monthly_income = float(input("Enter your monthly income: "))
+monthly_expenses = float(input("Enter your total monthly expenses: "))
+monthly_savings = monthly_income - monthly_expenses
+projected_savings = monthly_savings * 12 + (monthly_savings * 12 * 0.05)
 
-print(f"Your monthly savings are ${monthlySavings}.")
-print(f"Projected savings after one year, with interest, is: ${int(projectedSavings)}.")
+print(f"Your monthly savings are ${monthly_savings}.")
+print(f"Projected savings after one year, with interest, is: ${int(projected_savings)}.")
