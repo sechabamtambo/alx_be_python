6b05461562864d37986a92645ac8c32e8666c13b
Sechaba Mtambo <mtambosechaba@gmail.com>
2025-07-27 13:19:28 +0200

    Add datetime exploration script with current date display and future date calculation

---
+++ fns_and_dsa/explore_datetime.py
@@ -0,0 +1,14 @@
+from datetime import datetime, timedelta
+
+def display_current_datetime():
+    current_date = datetime.now()
+    return current_date
+
+print(f"Current date and time: {display_current_datetime()}")
+
+def calculate_future_date():
+    number_of_days = int(input("Enter the number of days to add to the current date: "))
+    future_date = display_current_datetime() + timedelta(days=number_of_days)
+    return f"The future date is: {future_date.strftime('%Y-%m-%d')}"
+
+print(calculate_future_date())
