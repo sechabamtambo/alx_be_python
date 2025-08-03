86a530489a285dfdf003a92a3e63e924618ee1df
Sechaba Mtambo <mtambosechaba@gmail.com>
2025-08-03 13:09:44 +0200

    Add BankAccount class and CLI for deposit, withdraw, display commands

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -15,5 +15,5 @@ class BankAccount:
         
         
     def display_balance(self):
-        print(f"Current Balance: ${self.account_balance}")
+        print(f"Current Balance: ${self.account_balance:.2f}")
         
